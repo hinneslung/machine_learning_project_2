d2440df40048faefa359eb3debbfc4aa2284e3c4
Hinnes <hinneslung@gmail.com>
2016-04-16 13:54:13 -0400

    Initial commit

---
+++ imports.py
@@ -0,0 +1,8 @@
+from numpy import *
+from pylab import *
+from util import *
+import datasets,binary,dt,gd,knn,linear,mlGraphics,multiclass,runClassifier
+
+from sklearn.tree import DecisionTreeClassifier
+import multiclass
+from datasets import *
